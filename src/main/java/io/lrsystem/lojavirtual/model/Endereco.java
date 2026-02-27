9364f8c5681d96cc6cb49e2ad5de4bd77793395f
Lucas Rodrigues de Oliveira <lucas-oliveira25@outlook.com>
2026-02-27 08:54:37 -0300

    feat: Criada entidade usuario e feito relacionamento com acesso

--- src/main/java/io/lrsystem/lojavirtual/model/Endereco.java
+++ src/main/java/io/lrsystem/lojavirtual/model/Endereco.java
@@ -1,5 +1,6 @@
 package io.lrsystem.lojavirtual.model;
 
+import io.lrsystem.lojavirtual.enums.TipoEndereco;
 import jakarta.persistence.*;
 
 @Entity
@@ -35,6 +36,17 @@ public class Endereco {
     @JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey
             (value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
     private Pessoa pessoa;
+
+    @Enumerated(EnumType.STRING)
+    private TipoEndereco tipoEndereco;
+
+    public TipoEndereco getTipoEndereco() {
+        return tipoEndereco;
+    }
+
+    public void setTipoEndereco(TipoEndereco tipoEndereco) {
+        this.tipoEndereco = tipoEndereco;
+    }
 
     public Long getId() {
         return id;
